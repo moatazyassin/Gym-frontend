fb7c3bb2e03d60d39f79435da7458dccae1d6b23
moatazyassin <moatazyassin95@gmail.com>
2023-01-07 17:17:07 +0200

    removed unwanted code

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,12 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
-  constructor() { }
+  constructor(private router: Router) { }
+  RouteToDashboard()
+  {
+    console.log("lol")
+    this.router.navigate(['./dashboard']);
+
+  }
 
 }
